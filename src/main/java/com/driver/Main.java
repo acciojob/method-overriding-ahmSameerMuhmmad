82302a593839faaceed2b8e055bddf7e176824ae
MUHMMAD SAMEER🥀🌼 <muhmmadsameer143@gmail.com>
2023-01-19 22:09:43 +0530

    solution1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        A a=new A();
+        B b=new B();
+        b.meth();
+    }
   
+}
+class A{
+    String meth() {
+        return null;
+    }
+}
+class B extends A{
+    String meth(){
+        return null;
+    }
+
 }
