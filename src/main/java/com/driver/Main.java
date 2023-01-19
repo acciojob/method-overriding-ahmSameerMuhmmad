8bb097f09df56f75805a14bb25c3f7469492c59c
MUHMMAD SAMEER🥀🌼 <muhmmadsameer143@gmail.com>
2023-01-19 23:14:31 +0530

    solution final

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -14,7 +14,7 @@ public class Main {
         }
     }
 
-    static class B extends A {
+    public static class B extends A {
        public String meth(){
             return "Method is overridden in Extendend class B";
         }
